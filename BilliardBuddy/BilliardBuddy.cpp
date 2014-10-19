0a22cf806edc9e50d149cabfa0526112bdcf617c
Brian Stock <bstock92@gmail.com>
2014-10-19 03:38:12 -0400

    Implemented reading calibration file  - read "calibrated.xml" for camera matrix and distortion coefficients from successful camera calibration  - display undistorted output  - TODO: specify calibrated xml filename in default.cfg config file

--- BilliardBuddy/BilliardBuddy.cpp
+++ BilliardBuddy/BilliardBuddy.cpp
@@ -214,6 +214,8 @@ static void read(const FileNode& node, Settings& x, const Settings& default_valu
 
 enum { DETECTION = 0, CAPTURING = 1, CALIBRATED = 2 };
 
+void loadCameraParams(string filename, Mat& cameraMatrix, Mat& distCoeffs);
+
 bool runCalibrationAndSave(Settings& s, Size imageSize, Mat&  cameraMatrix, Mat& distCoeffs,
 	vector<vector<Point2f> > imagePoints);
 
@@ -241,6 +243,8 @@ int main(int argc, char* argv[])
 	Mat cameraMatrix, distCoeffs;
 	Size imageSize;
 	int mode = s.inputType == Settings::IMAGE_LIST ? CAPTURING : DETECTION;
+	loadCameraParams("calibrated.xml", cameraMatrix, distCoeffs);
+	mode = CALIBRATED;
 	clock_t prevTimestamp = 0;
 	const Scalar RED(0, 0, 255), GREEN(0, 255, 0);
 	const char ESC_KEY = 27;
@@ -252,67 +256,13 @@ int main(int argc, char* argv[])
 
 		view = s.nextImage();
 
-		//-----  If no more image, or got enough, then stop calibration and show result -------------
-		if (mode == CAPTURING && imagePoints.size() >= (unsigned)s.nrFrames)
-		{
-			if (runCalibrationAndSave(s, imageSize, cameraMatrix, distCoeffs, imagePoints))
-				mode = CALIBRATED;
-			else
-				mode = DETECTION;
-		}
-		if (view.empty())          // If no more images then run calibration, save and stop loop.
-		{
-			if (imagePoints.size() > 0)
-				runCalibrationAndSave(s, imageSize, cameraMatrix, distCoeffs, imagePoints);
-			break;
-		}
-
 
 		imageSize = view.size();  // Format input image.
 		if (s.flipVertical)    flip(view, view, 0);
 
 		vector<Point2f> pointBuf;
 
-		bool found;
-		switch (s.calibrationPattern) // Find feature points on the input format
-		{
-		case Settings::CHESSBOARD:
-			found = findChessboardCorners(view, s.boardSize, pointBuf,
-				CV_CALIB_CB_ADAPTIVE_THRESH | CV_CALIB_CB_FAST_CHECK | CV_CALIB_CB_NORMALIZE_IMAGE);
-			break;
-		case Settings::CIRCLES_GRID:
-			found = findCirclesGrid(view, s.boardSize, pointBuf);
-			break;
-		case Settings::ASYMMETRIC_CIRCLES_GRID:
-			found = findCirclesGrid(view, s.boardSize, pointBuf, CALIB_CB_ASYMMETRIC_GRID);
-			break;
-		default:
-			found = false;
-			break;
-		}
 
-		if (found)                // If done with success,
-		{
-			// improve the found corners' coordinate accuracy for chessboard
-			if (s.calibrationPattern == Settings::CHESSBOARD)
-			{
-				Mat viewGray;
-				cvtColor(view, viewGray, COLOR_BGR2GRAY);
-				cornerSubPix(viewGray, pointBuf, Size(11, 11),
-					Size(-1, -1), TermCriteria(CV_TERMCRIT_EPS + CV_TERMCRIT_ITER, 30, 0.1));
-			}
-
-			if (mode == CAPTURING &&  // For camera only take new samples after delay time
-				(!s.inputCapture.isOpened() || clock() - prevTimestamp > s.delay*1e-3*CLOCKS_PER_SEC))
-			{
-				imagePoints.push_back(pointBuf);
-				prevTimestamp = clock();
-				blinkOutput = s.inputCapture.isOpened();
-			}
-
-			// Draw the corners.
-			drawChessboardCorners(view, s.boardSize, Mat(pointBuf), found);
-		}
 
 		//----------------------------- Output Text ------------------------------------------------
 		string msg = (mode == CAPTURING) ? "100/100" :
@@ -355,27 +305,6 @@ int main(int argc, char* argv[])
 		{
 			mode = CAPTURING;
 			imagePoints.clear();
-		}
-	}
-
-	// -----------------------Show the undistorted image for the image list ------------------------
-	if (s.inputType == Settings::IMAGE_LIST && s.showUndistorsed)
-	{
-		Mat view, rview, map1, map2;
-		initUndistortRectifyMap(cameraMatrix, distCoeffs, Mat(),
-			getOptimalNewCameraMatrix(cameraMatrix, distCoeffs, imageSize, 1, imageSize, 0),
-			imageSize, CV_16SC2, map1, map2);
-
-		for (int i = 0; i < (int)s.imageList.size(); i++)
-		{
-			view = imread(s.imageList[i], 1);
-			if (view.empty())
-				continue;
-			remap(view, rview, map1, map2, INTER_LINEAR);
-			imshow("Image View", rview);
-			char c = (char)waitKey();
-			if (c == ESC_KEY || c == 'q' || c == 'Q')
-				break;
 		}
 	}
 
@@ -461,6 +390,15 @@ static bool runCalibration(Settings& s, Size& imageSize, Mat& cameraMatrix, Mat&
 		rvecs, tvecs, cameraMatrix, distCoeffs, reprojErrs);
 
 	return ok;
+}
+
+// Load the specified calibration file and store camera matrix and distortion coefficients matrix
+void loadCameraParams(string filename, Mat& cameraMatrix, Mat& distCoeffs)
+{
+	FileStorage fs(filename, FileStorage::READ);
+	fs["Camera_Matrix"] >> cameraMatrix;
+	fs["Distortion_Coefficients"] >> distCoeffs;
+	fs.release();
 }
 
 // Print camera parameters to the output file
