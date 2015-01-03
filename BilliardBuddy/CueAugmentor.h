3006461d1c4a07077d47b7080a2f5ed4456f0517
Brian Stock <bstock92@gmail.com>
2015-01-03 10:16:43 -0500

    Removed reference to non-existant header file resource.h

---
+++ BilliardBuddy/CueAugmentor.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "VisualAugmentor.h"
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+
+class CueAugmentor :
+	public virtual VisualAugmentor
+{
+private:
+
+public:
+	CueAugmentor();
+	~CueAugmentor();
+	void CueAugmentor::augment(cv::Mat& frame, cv::vector<cv::Vec2i> points);
+
+};
+
